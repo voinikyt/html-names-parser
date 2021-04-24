68550670a8e46eb589d8a398d265fa01f92daaa1
Ivaylo Kolev <ivaylo.kolev.ext@racingpost.com>
2021-04-24 18:24:55 +0300

    Making tests pass using WireMock

--- core/src/test/java/com/ontotext/intervirew/core/parser/URLPersonParserTest.java
+++ core/src/test/java/com/ontotext/intervirew/core/parser/URLPersonParserTest.java
@@ -1,22 +1,37 @@
 package com.ontotext.intervirew.core.parser;
 
-import static org.junit.Assert.*;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.List;
-
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.core.Options;
+import com.ontotext.intervirew.core.entity.Person;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
-import com.ontotext.intervirew.core.entity.Person;
-import com.ontotext.intervirew.core.parser.DocumentParserException;
-import com.ontotext.intervirew.core.parser.URLPersonParser;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static org.junit.Assert.assertTrue;
 
 public class URLPersonParserTest {
 
 	@Test
-	public void testWithHttpURL() throws MalformedURLException {
-		URLPersonParser parser = new URLPersonParser(new URL("http://store4.data.bg/ruin/tasks/B.html"));		
+	public void testWithHttpURL() throws IOException, URISyntaxException {
+		String html = IOUtils.toString(getClass().getResource("/B.html").toURI(), StandardCharsets.UTF_8);
+
+		WireMockServer server = new WireMockServer(Options.DYNAMIC_PORT);
+		server.start();
+		server.stubFor(
+				WireMock.get(urlEqualTo("/file"))
+						.willReturn(aResponse()
+								.withBody(html)));
+
+		URLPersonParser parser = new URLPersonParser(new URL("http://localhost:" + server.port() + "/file"));
 		parser.firstNameColumnIndex(2);
 		parser.lastNameColumnIndex(1);
 		List<Person> people = parser.parse();
@@ -24,12 +39,13 @@ public class URLPersonParserTest {
 	}
 	
 	@Test(expected = DocumentParserException.class)
-	public void testWithFileSystemURL() throws MalformedURLException {
+	public void testWithFileSystemURL() {
 		URL fileURL = getClass().getResource("/B.html");
 		URLPersonParser parser = new URLPersonParser(fileURL);		
 		parser.firstNameColumnIndex(2);
-		parser.lastNameColumnIndex(1);		
-		parser.parse();
+		parser.lastNameColumnIndex(1);
+		List<Person> people = parser.parse();
+		assertTrue(people.size() > 0);
 	}
 
 }
--- shell/src/test/java/com/ontottext/interview/cltool/MainTest.java
+++ shell/src/test/java/com/ontottext/interview/cltool/MainTest.java
@@ -1,61 +1,77 @@
 package com.ontottext.interview.cltool;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.junit.Assert.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.core.Options;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
-public class MainTest {	
-	
-	@Test
-	public void testMainWithFile() throws IOException {
-		String filePathA = JCommanderTest.class.getResource("/A.html").getFile();						
-		File destination = File.createTempFile("destination", ".rdf");				
-		
-		assertTrue(destination.length() == 0);		
-		
-		String[] parameters = new String[] {
-				"-file", filePathA,				
-				"-firstNameColumn", "2",
-				"-lastNameColumn", "3",
-				"--destination", destination.getAbsolutePath(),
-				"-format", "TURTLE"				
-		};				
-		int result = Main.mainMethod(parameters);
-		
-		assertEquals(0, result);
-		assertTrue(destination.length() > 0);		
-	}
-	
-	@Test
-	public void testMainWithURL() throws IOException {							
-		File destination = File.createTempFile("destination", ".rdf");				
-		
-		assertTrue(destination.length() == 0);		
-		
-		String[] parameters = new String[] {
-				"-url", "http://store4.data.bg/ruin/tasks/A.html",				
-				"-firstNameColumn", "2",
-				"-lastNameColumn", "3",
-				"--destination", destination.getAbsolutePath(),
-				"-format", "TURTLE"				
-		};				
-		int result = Main.mainMethod(parameters);
-		
-		assertEquals(0, result);
-		assertTrue(destination.length() > 0);		
-	}
-	
-	@Test
-	public void testUsage() throws IOException {						
-		String[] parameters = new String[] {
-				"-help"				
-		};				
-		int result = Main.mainMethod(parameters);
-		
-		assertEquals(0, result);			
-	}
+public class MainTest {
+
+    @Test
+    public void testMainWithFile() throws IOException {
+        String filePathA = JCommanderTest.class.getResource("/A.html").getFile();
+        File destination = File.createTempFile("destination", ".rdf");
+
+        assertTrue(destination.length() == 0);
+
+        String[] parameters = new String[]{
+                "-file", filePathA,
+                "-firstNameColumn", "2",
+                "-lastNameColumn", "3",
+                "--destination", destination.getAbsolutePath(),
+                "-format", "TURTLE"
+        };
+        int result = Main.mainMethod(parameters);
+
+        assertEquals(0, result);
+        assertTrue(destination.length() > 0);
+    }
+
+    @Test
+    public void testMainWithURL() throws IOException, URISyntaxException {
+        String html = IOUtils.toString(getClass().getResource("/A.html").toURI(), StandardCharsets.UTF_8.displayName());
+
+        WireMockServer server = new WireMockServer(Options.DYNAMIC_PORT);
+        server.start();
+        server.stubFor(
+                WireMock.get(urlEqualTo("/file"))
+                        .willReturn(aResponse()
+                                .withBody(html)));
+
+        File destination = File.createTempFile("destination", ".rdf");
+        assertTrue(destination.length() == 0);
+
+        String[] parameters = new String[]{
+                "-url", "http://localhost:" + server.port() + "/file",
+                "-firstNameColumn", "2",
+                "-lastNameColumn", "3",
+                "--destination", destination.getAbsolutePath(),
+                "-format", "TURTLE"
+        };
+        int result = Main.mainMethod(parameters);
+
+        assertEquals(0, result);
+        assertTrue(destination.length() > 0);
+    }
+
+    @Test
+    public void testUsage() {
+        String[] parameters = new String[]{
+                "-help"
+        };
+        int result = Main.mainMethod(parameters);
+
+        assertEquals(0, result);
+    }
 
 }
